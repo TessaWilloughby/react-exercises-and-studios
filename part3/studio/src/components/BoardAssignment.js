25cf7c77da644096010f52b5dcc8a42eeaa01e49
Tessa Willoughby <tessa.willoughby@me.com>
2023-08-10 23:30:17 -0400

    created components with states and handlers

--- part3/studio/src/components/BoardAssignment.js
+++ part3/studio/src/components/BoardAssignment.js
@@ -1,15 +1,41 @@
 import { useState } from 'react';
 
 export default function BoardAssignment () {
-   const boards = [];
+
+   const [boardName, setBoardName] = useState('no boards yet');
+
+   const boards = [
+      {
+         label: "End of Unit Ideas",
+         value: "unitIdeas"
+      },
+
+      { 
+         label: "End of Summer Party",
+         value: "summerParty"
+      }, 
+
+      {
+         label: "Cold Drinks",
+         value: "coldDrinks"
+      }
+   ];
+
+   let boardOptions = boards.map((board, index) => {
+      return <option key={index} value={board.value}>{board.label}</option>
+   });
+
+
 
    const handleChange = (event) => {
+      setBoardName(event.target.value);
    }
 
    return (
       <div style={{paddingTop: "50px"}}>
       <label>Save to Board: </label>
       <select value={boardName} onChange={handleChange}>
+         {boardOptions}
       </select>
 
       <p>Saved to {boardName}!</p>
